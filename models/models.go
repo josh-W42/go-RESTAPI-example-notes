ab3f5faa97ebbdff3f61bfc7aa54363791221545
jwilson42 <wilson.jd022@gmail.com>
2021-07-06 15:48:33 -0700

    added one basic get route and some date models

---
+++ models/models.go
@@ -0,0 +1,42 @@
+package models
+
+import "time"
+
+/*
+	Here is where we'll store all the
+	data models for this app.
+
+	we could go a step further and create
+	individual packages for each model but
+	for these purposes we'll limit to one.
+*/
+
+type Movie struct {
+	ID          int          `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	Year        int          `json:"year"`
+	ReleaseDate time.Time    `json:"release_date"`
+	Runtime     int          `json:"runtime"`
+	Rating      int          `json:"rating"`
+	MPAARating  string       `json:"mpaarating"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	MovieGenre  []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+	ID        int       `json:"id"`
+	GenreName string    `json:"genre_name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type MovieGenre struct {
+	ID        int       `json:"id"`
+	MovieId   int       `json:"movie_id"`
+	GenreId   int       `json:"genre_id"`
+	Genre     Genre     `json:"genre"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
